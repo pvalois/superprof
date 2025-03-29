77b6aad902c13e62a48eebc8f82f96cffa215611
Pascal Valois <pvalois@email.com>
2025-03-29 19:46:38 +0100

    Add: exemples de scripts

---
+++ Exemples_de_Scripts/arithmetique.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+a=15
+b=18 
+
+echo $((a+b)) # Addition
+echo $((b-a)) # Soustraction
+echo $((a*b)) # Multiplication
+echo $((b/a)) # Division Entière
+echo $((b%a)) # Reste de division Entière
